c3c192d449e1562a3e197c86c5dfcd80f193ba65
AdriAntich <adriantich@gmail.com>
2023-05-10 12:42:47 +0200

    kindom matched also with superkingdom

--- R/mjolnir5_THOR.R
+++ R/mjolnir5_THOR.R
@@ -867,7 +867,7 @@ mjolnir5_THOR <- function(lib,cores,tax_dir,tax_dms_name=NULL,obipath="",run_eco
     taxa <- c("superkingdom_name","kingdom_name","phylum_name","class_name",
               "order_name","family_name","genus_name","species_name")
     # trace those taxa with na info in higher taa than assigned. If root there's nothing higher
-    na_taxa <- taxa[ifelse(matrix.data["rank",2]=="root",0,1:grep(matrix.data["rank",2],taxa))]
+    na_taxa <- taxa[ifelse(matrix.data["rank",2]=="root",0,1:which(matrix.data["rank",2]==taxa))]
     na_taxa <- na_taxa[is.na(matrix.data[na_taxa,2])]
 
     matrix.data[na_taxa,2] <- "Correct_manually"
